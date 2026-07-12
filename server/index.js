415b77a9d6da72a9b75b5f38c7d8e03b3cb737d6
Shuiii <72098121+shuiiiiiii@users.noreply.github.com>
2026-07-12 20:49:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,30 +1,25 @@
-// const express = require("express");
-// const app = express();
-// const PORT = 6767;
-
-// const indexRoutes = require("./routes/indexRoutes");
-
-// app.set("view engine", "ejs");
-// app.use("/static", express.static("public"));
-
-// app.use(indexRoutes);
-
-// app.listen(PORT || process.env.PORT, process.env.IP, () => {
-//     console.log("Server is up on PORT: 6767");
-// });
-
 const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 6767;
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+
+dotenv.config();
+
+connectDB();
 
 const indexRoutes = require("./routes/indexRoutes");
+const galleryRoutes = require("./routes/gallery.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
 
 app.use(indexRoutes);
+app.use(galleryRoutes);
 
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
@@ -33,4 +28,3 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 module.exports = app;
-
